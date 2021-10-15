3d11a343fa1099a1efb3e733ef475ddf912018ab
fcarlosmonteiro <fcarlosmonteiro@gmail.com>
2021-10-15 16:23:50 -0300

    improve segmentation

--- segmentation.py
+++ segmentation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+
 def rescale(src):
     #percent by which the image is resized
     scale_percent = 30
@@ -20,22 +21,31 @@ def rescale(src):
 i = cv2.imread('i4.jpg')
 imagem=rescale(i)
 
-original = imagem.copy()
-imagem1 = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
-#cv2.imshow("color",imagem1)
+image = cv2.fastNlMeansDenoisingColored(imagem,None,12,12,7,21)
+#cv2.imshow("desnoise",image)
 
-imagem = cv2.GaussianBlur(imagem1,(95,95),cv2.BORDER_DEFAULT)
+ds = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
+#cv2.imshow("color after desnoise",ds)
 
-#cv2.imshow("Guassian2",imagem)
+img_hsv = cv2.GaussianBlur(ds,(95,95),cv2.BORDER_DEFAULT)
 
-lower = np.array([0,30,0], dtype="uint8")
-upper = np.array([46,255,255], dtype="uint8")
-mascara = cv2.inRange(imagem, lower, upper)
+result = img_hsv.copy()
+image = cv2.cvtColor(img_hsv, cv2.COLOR_BGR2HSV)
+lower = np.array([0,10,10])
+upper = np.array([56,255,255])
+mask = cv2.inRange(img_hsv, lower, upper)
+result = cv2.bitwise_and(result, result, mask=mask)
 
-cnts = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+#cv2.imshow('mask', mask)
+#cv2.imshow('result in gray', gray)
+
+edge_detected_image = cv2.Canny(gray, 175, 200)
+cv2.imshow('Edge', edge_detected_image)
+
+cnts = cv2.findContours(edge_detected_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-#for c in cnts:
 #seleciona o contorno maior
 c = max(cnts, key = cv2.contourArea)
 
@@ -44,15 +54,14 @@ x,y,w,h = cv2.boundingRect(c)
 print([x,y,w,h])
 
 #plota na imagem
-cv2.rectangle(original, (x, y), (x + w, y + h), (36,255,12), 2)
-cv2.imshow('original', original)
+cv2.rectangle(imagem, (x, y), (x + w, y + h), (36,255,12), 2)
+cv2.imshow('original', imagem)
 
-cv2.rectangle(mascara, (x, y), (x + w, y + h), (36,255,12), 2)
-cv2.imshow('mascara', mascara)
+cv2.rectangle(result, (x, y), (x + w, y + h), (36,255,12), 2)
+cv2.imshow('result', result)
 
 #calcula o moments somente para o contorno maior
 M = cv2.moments(c)
 print(M)
-
 
 cv2.waitKey()
